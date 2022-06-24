114ad5cb2428d8c8aa2480c36e35374d41a84804
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-06-24 12:33:59 +1000

    add summarise.py, add to run_interaction.py
    Co-authored-by: Michael Franklin <illusional@users.noreply.github.com>

---
+++ summarise.py
@@ -0,0 +1,18 @@
+"""
+
+Task A:
+    cwd: Gene A:
+    /Users/anna/cromwell-workfig-dir/workflow-id/workflonwame/taskname/shard-1/execution/
+
+    cwd: Gene X
+    /Users/anna/cromwell-workfig-dir/workflow-id/workflonwame/taskname/shard-25/execution/
+
+
+Summarise Task:
+    cwd: TaskName/execution
+    inputs:
+        .../TaskName/inputs/12jasd123/gene1.txt
+        .../TaskName/inputs/12u6i98ca/gene2.txt
+"""
+
+# os.path.splitext(os.path.basename("/big/long/myfilename.txt"))[0] # myfilename
