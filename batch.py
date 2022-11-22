81ce46bb3ee531e9c91b21b93c3ac806f9340022
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-11-23 10:25:15 +1100

    begin adding to hail-batch workflow

--- batch.py
+++ batch.py
@@ -1,4 +1,17 @@
+#!/usr/bin/env python3
+# pylint: disable=wrong-import-position,import-error 
+
+"""
+Hail Batch workflow for the rare-variant association analysis, including:
+
+- get relevant variants around a gene and export genotypes as plink files
+- generate input files for association tests 
+- run association tests
+"""
+
+# import python modules # is there a specific order, rationale for grouping imports??
 import logging
+import click
 import hail as hl
 import hailtop.batch as hb
 import pandas as pd
@@ -7,75 +20,39 @@ from cpg_utils.hail_batch import get_config, remote_tmpdir
 from cloudpathlib import AnyPath
 from cpg_utils.hail_batch import dataset_path, init_batch, output_path
 
-config = get_config()
+# use logging to print statements, display at info level
+logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.INFO)
 
-def main(
-    # genotypeFiles: dict[str, str], # one file per chromosome
-    # genotypeFilesBims: dict[str, str],
-    # genotypeFilesFams: dict[str, str],
-    # phenotypeFiles: dict[str, str],
-    # mafFiles: dict[str, str],
-    # contextFile: str,
-    # kinshipFile: str,
-    # sampleMappingFile: str,
-    # featureVariantFile: str,
-    # nContexts: int=10,
-    # fdrThreshold: int=1,
-    mt_path: str,               # 'mt/v7.mt'
-    vep_ht_path: str,           # 'tob_wgs_vep/104/vep104.3_GRCh38.ht'
-    gene_loc_files: list[str],
-):
+DEFAULT_JOINT_CALL_MT = dataset_path('mt/v7.mt')
+DEFAULT_ANNOTATION_HT = dataset_path('tob_wgs_vep/104/vep104.3_GRCh38.ht')  # atm VEP
 
-    MT = dataset_path(mt_path)
-    VEP_HT = dataset_path(vep_ht_path)
-
-    sb = hb.ServiceBackend(
-    billing_project=config['hail']['billing_project'],
-    remote_tmpdir=remote_tmpdir(),
-)
-    batch = hb.Batch("CellRegMap pipeline", backend=sb)
-
-    # determine for each chromosome
-
-    plink_job_reference_and_outputs_for_gene: dict[str, tuple[hb.Job, str]] = {}
-    # if you know the output, you need to wait for the actual job that processes it
-    # so store the reference to the job that's producing the file
-    
-    for chr_loc_file in gene_loc_files:
-        with AnyPath(chr_loc_file).open() as f:
-            for line in f:
-                # i have no idea, I'm just randomly guessing
-                gene = line.split("\t")[0]
-                job = batch.new_python_job(f"Preprocess: {gene}")
-
-                plink_output_prefix = output_path(f'plink_files/{gene}_rare_promoter')
-                plink_job_reference_and_outputs_for_gene[gene] = (job, plink_output_prefix)
-                result = job.call(get_promoter_variants, gene_file=chr_loc_file, gene_name=gene, window_size=50_000, plink_output_prefix=plink_output_prefix)
-
-
-
-    _gene = "BRCA1"
-    dependent_job, output_prefix = plink_job_reference_and_outputs_for_gene[_gene]
-    downstream_job = batch.new_job("Downstream job")
-    downstream_job.depends_on(dependent_job)
-
-        # run that function
-            
-
-
-
+# region GET_RELEVANT_VARIANTS
 
 def get_promoter_variants(
-    MT: str,
-    VEP_HT: str,
+    mt_path: str,
+    ht_path: str,
     gene_file: str,
     gene_name: str,
     window_size: int,
-    plink_output_prefix: str,
+    # plink_output_prefix: str, # figure out how to deal with this
 ):
+    """Subset hail matrix table
+
+    Input:
+    gene_file: path to a tsv file with information on
+    the chrom, start and end location of genes (rows)
+
+    Output:
+    For retained variants, that are: 1) not ref-only, 2) biallelic, 
+    3) meet QC filters, 4) rare (MAF<5%), 5) in promoter regions and
+    6) within 50kb up or down-stream of the gene body (or in the gene body itself)
+    returns:
+    - Path to plink prefix for genotype files (plink format: .bed, .bim, .fam)
+    - Path to hail table with variant (rows) stats for downstream analyses
+    """
     # read hail matrix table object (WGS data)
     init_batch()
-    mt = hl.read_matrix_table(MT)
+    mt = hl.read_matrix_table(mt_path)
 
     # get relevant chromosome
     gene_df = pd.read_csv(AnyPath(dataset_path(gene_file)), sep='\t', index_col=0)
@@ -119,7 +96,7 @@ def get_promoter_variants(
     )
 
     # annotate using VEP
-    vep_ht = hl.read_table(VEP_HT)
+    vep_ht = hl.read_table(ht_path)
     mt = mt.annotate_rows(vep=vep_ht[mt.row_key].vep)
     mt_path = output_path('densified_qced_snps_only_vep_annotated.mt', 'tmp')
     mt = mt.checkpoint(
@@ -155,3 +132,99 @@ def get_promoter_variants(
     # export MT object to PLINK (promoter variants)
     mt_path = output_path(f'plink_files/{gene_name}_rare_promoter')
     export_plink(mt, mt_path, ind_id=mt.s)
+
+    return [mt_path, ht_filename]
+
+# endregion GET_RELEVANT_VARIANTS
+
+# region PREPARE_INPUT_FILES
+
+def prepare_input_files():
+    """Prepare association test input files
+
+    Input:
+    genotype: plink files
+    phenotype: gene expression
+
+    Output:
+    genotype matrix
+    phenotype vector
+    """
+
+# endregion PREPARE_INPUT_FILES
+
+# region RUN_ASSOCIATION
+
+def run_gene_association():
+    """Run gene-set association test
+
+    Input:
+    input files
+
+    Output:
+    list of p-values from the three tests
+    """
+
+# endregion RUN_ASSOCIATION
+
+
+config = get_config()
+
+
+@click.command()
+@click.option('--gene-list')
+def main(
+    # genotypeFiles: dict[str, str], # one file per chromosome
+    # genotypeFilesBims: dict[str, str],
+    # genotypeFilesFams: dict[str, str],
+    # phenotypeFiles: dict[str, str],
+    # mafFiles: dict[str, str],
+    # contextFile: str,
+    # kinshipFile: str,
+    # sampleMappingFile: str,
+    # featureVariantFile: str,
+    # nContexts: int=10,
+    # fdrThreshold: int=1,
+    mt_path: str,               # 'mt/v7.mt'
+    vep_ht_path: str,           # 'tob_wgs_vep/104/vep104.3_GRCh38.ht'
+    gene_loc_files: list[str],
+):
+
+    
+
+    sb = hb.ServiceBackend(
+    billing_project=config['hail']['billing_project'],
+    remote_tmpdir=remote_tmpdir(),
+)
+    batch = hb.Batch("CellRegMap pipeline", backend=sb)
+
+    # determine for each chromosome
+
+    plink_job_reference_and_outputs_for_gene: dict[str, tuple[hb.Job, str]] = {}
+    # if you know the output, you need to wait for the actual job that processes it
+    # so store the reference to the job that's producing the file
+    
+    for chr_loc_file in gene_loc_files:
+        with AnyPath(chr_loc_file).open() as f:
+            for line in f:
+                # i have no idea, I'm just randomly guessing
+                gene = line.split("\t")[0]
+                job = batch.new_python_job(f"Preprocess: {gene}")
+
+                plink_output_prefix = output_path(f'plink_files/{gene}_rare_promoter')
+                plink_job_reference_and_outputs_for_gene[gene] = (job, plink_output_prefix)
+                result = job.call(get_promoter_variants, gene_file=chr_loc_file, gene_name=gene, window_size=50_000, plink_output_prefix=plink_output_prefix)
+
+
+
+    _gene = "BRCA1"
+    dependent_job, output_prefix = plink_job_reference_and_outputs_for_gene[_gene]
+    downstream_job = batch.new_job("Downstream job")
+    downstream_job.depends_on(dependent_job)
+
+        # run that function
+            
+
+
+
+
