bd2151caf40ef634b656bd72c06ab1a7e398cd24
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-11-29 15:44:53 +1100

    Hail batch part1 (#6)
    * initial commit for batch workflow
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * add imports'
    
    * start working on batch.py and add plan to readme
    
    * update README
    
    * update README
    
    * begin adding to hail-batch workflow
    
    * add crm runner, add to overall structure
    
    * add p-value matrix
    
    * add to main taking inspo from eqtl pipeline
    
    * add comments where i am a bit stuck
    
    * start working on prepare inputs
    
    * continue working on prepare inputs
    
    * done with prepare inputs
    
    * try scattering
    
    * remove old useless files
    
    * split hail matrix table operations between common and gene-specific, thanks Matt
    
    * add specified output path, but unsure of syntax
    
    * remove return brackets
    
    * add checkpoints
    
    * check promoter exists in list of biotypes
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * add to requirements
    
    * rename filter function, add to main
    
    * add to job
    
    * add to batch.py
    
    * add example paths as comments
    
    * Update batch.py
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * incorporate Matt's suggestions
    
    * add dict method
    
    * Update README.md
    
    * swap loops over cell types, genes
    
    * start adding dependencies between jobs
    
    * add cellregmap image
    
    * attempt to change to gene dict
    
    * continue to change to gene dict
    
    * continue to work on workflow
    
    * add inputs to main
    
    * add plink, pv as gene attributes
    
    * clean and fix not-defined / referenced variables
    
    * fix pv output file
    
    * remove some comments, format with black
    
    * attempt to concatenate chrom dictionaries using update
    
    * make window size customisable
    
    * only first part of hail batch pipeline
    
    * only first part of hail batch pipeline
    
    * only first part of hail batch pipeline for PR
    
    * remove celltype from plink path
    
    * Update README.md
    
    * Update README.md
    
    * Update README.md
    
    * Update batch.py
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * add batch run
    
    * fix ouput path
    
    * format with black
    
    * Update batch.py
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * Update README.md
    
    * specify image
    
    * Update README.md
    
    * comment out cellregmap for now
    
    * comment out actual runner
    
    * Update README.md
    
    * add back in for review
    
    * remove unused functions / imports to simplify PR
    
    * Update batch.py
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * Update batch.py
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    
    * Update batch.py
    
    Co-authored-by: Leonhard Gruenschloss <leonhard@gruenschloss.org>
    
    * rename output path
    
    * remove R stuff at this stage
    
    * update image def
    
    * remove concatenating jobs at this stage
    
    * add defaults to click
    
    * chechlpoint to write
    
    * checkpoint to write
    
    * comment out old image line
    
    Co-authored-by: Matt Welland <mattwellie@gmail.com>
    Co-authored-by: Leonhard Gruenschloss <leonhard@gruenschloss.org>

--- README.md
+++ README.md
@@ -1,6 +1,54 @@
-# CellRegMap_pipeline
-WDL workflow to facilitate running [CellRegMap](https://github.com/limix/CellRegMap).
+# CellRegMap pipeline
 
+## Usage overview
+
+In its original version, this is a WDL workflow to facilitate running [CellRegMap](https://github.com/limix/CellRegMap).
+
+[CellRegMap v2](https://github.com/annacuomo/CellRegMap) (new name to come) now comes with new functionalities, particularly around the implementation of tests to map effects of rare genetic variants (called from whole-genome sequencing, WGS).
+To assess these new methods, this pipeline needs to be run on both real and simulated data.
+
+For simulations, the pipeline also needs to run alternative methods (including the [SKAT](https://github.com/leelabsg/SKAT)- and [ACAT](https://github.com/yaowuliu/ACAT)-derived methods) for comparison purposes, which are implemented in R.
+
+For real data, the pipeline also needs to use Hail Query to query genetic variants and annotations stored as Hail Matrix Tables and Hail Tables.
+
+### Structure of repo:
+* WDL workflow 
+  * this does not contain input files generation (does not require Hail Query) and takes pre-generated input files
+  * should work for both simulated and real data (see below)
+  * contains both python and R scripts as tasks (and respective Docker images)
+  * returns p-values and other summary statistics for all results (original CellRegMap, new CellRegMap, R-implemented tests) 
+* Hail Batch workflow
+  * single-script end-to-end (Python) workflow from data collection / input file generation (using Hail Query) to association testing to returning summary stats (for new CellRegMap specifically, to run internally)
+* (Python) scripts to generate both real and simulated input files to feed to the WDL workflow
+
+## Hail Batch workflow
+Single [python script](batch.py), key steps (implemented as distinct functions):
+* preselection of full WGS hail matrix table to includ only biallelic (n alleles=2) rare (freq<5%) SNVs
+* given one gene and the prefiltered WGS hail matrix table, selects relevant variants and export as plink files
+* plink files are read in and turned into genotype input files, other input files (phenotype, kinship) are processed
+* genes are parallelised into chunks
+* CellRegMap-RV various tests are run (each chunk independently)
+* resulting p-values are aggregated across jobs and saved
+
+To run:
+```
+analysis-runner \
+    --dataset tob-wgs \
+    --access-level test \
+    --output-dir "tob_wgs_rv/pseudobulk_rv_association" \
+    --image australia-southeast1-docker.pkg.dev/cpg-common/images/cellregmap:0.0.3 \
+    --description "CellRegMap batch job" \
+    python3 batch.py \
+      --expression-file-prefix scrna-seq/grch38_association_files \
+      --sample-mapping-file scrna-seq/grch38_association_files/OneK1K_CPG_IDs.tsv \
+      --genes VPREB3 \
+      --chromosomes 22 \
+      --cell-types B_intermediate
+```
+
+## CellRegMap pipeline v1
+
+A WDL workflow to facilitate running [CellRegMap](https://github.com/limix/CellRegMap).
 A container to run the workflow is available [on Dockerhub](https://hub.docker.com/repository/docker/annasecuomo/cellregmap_pipeline).
 
 ## To run on a High Performance Computing (HPC) system
---
+++ batch.py
@@ -0,0 +1,323 @@
+#!/usr/bin/env python3
+# pylint: disable=wrong-import-position,import-error
+
+"""
+Hail Batch workflow for the rare-variant association analysis, including:
+
+- get relevant variants around a gene and export genotypes as plink files
+- generate input files for association tests 
+- run association tests
+"""
+
+# import python modules
+import os
+import re
+
+import click
+import logging
+
+from cloudpathlib import AnyPath
+from cpg_utils import to_path
+from cpg_utils.hail_batch import (
+    dataset_path,
+    get_config,
+    init_batch,
+    output_path,
+    remote_tmpdir,
+)
+
+import numpy as np
+import pandas as pd
+import xarray as xr
+
+from limix.qc import quantile_gaussianize
+from numpy import eye, ones
+from pandas_plink import read_plink1_bin
+from scipy.stats import shapiro
+
+# qvalue imports
+import scipy as sp
+from scipy import interpolate
+
+# make_gene_loc_dict import
+from csv import DictReader
+
+import hail as hl
+import hailtop.batch as hb
+from hail.methods import export_plink
+
+# use logging to print statements, display at info level
+logging.basicConfig(
+    format="%(levelname)s:%(message)s", level=logging.INFO
+)  # consider removing some print statements?
+
+DEFAULT_JOINT_CALL_MT = dataset_path("mt/v7.mt")
+DEFAULT_ANNOTATION_HT = dataset_path(
+    "tob_wgs_vep/104/vep104.3_GRCh38.ht"
+)  # atm VEP only
+
+
+CELLREGMAP_IMAGE = get_config()["workflow"][
+    "driver_image"
+]  # australia-southeast1-docker.pkg.dev/cpg-common/images/cellregmap:0.0.3
+
+# region SUBSET_VARIANTS
+
+
+def filter_variants(
+    mt_path: str,  # "mt/v7.mt"
+    samples: list[str],
+    output_mt_path: str,  # "tob_wgs_rv/densified_rv_only.mt"
+):
+    """Subset hail matrix table
+
+    Input:
+    joint call hail matrix table
+    set of samples for which we have scRNA-seq data
+
+    Output:
+    subset hail matrix table, containing only variants that:
+    1) are not ref-only, 2) biallelic, 3) meet QC filters, 4) are rare (MAF<5%)
+    """
+    # read hail matrix table object (WGS data)
+    init_batch()
+    mt = hl.read_matrix_table(mt_path)
+
+    # subset to relevant samples (samples we have scRNA-seq data for)
+    mt = mt.filter_cols(hl.set(samples).contains(mt.s))
+
+    # densify (can this be done at the end?)
+    mt = hl.experimental.densify(mt)
+
+    # filter out low quality variants and consider biallelic variants only (no multi-allelic, no ref-only)
+    mt = mt.filter_rows(  # check these filters!
+        (hl.len(hl.or_else(mt.filters, hl.empty_set(hl.tstr))) == 0)  # QC
+        & (hl.len(mt.alleles) == 2)  # remove hom-ref
+        & (mt.n_unsplit_alleles == 2)  # biallelic
+        & (hl.is_snp(mt.alleles[0], mt.alleles[1]))  # SNVs
+    )
+
+    # filter rare variants only (MAF < 5%)
+    mt = hl.variant_qc(mt)
+    mt = mt.filter_rows(
+        (mt.variant_qc.AF[1] < 0.05) & (mt.variant_qc.AF[1] > 0)
+        | (mt.variant_qc.AF[1] > 0.95) & (mt.variant_qc.AF[1] < 1)
+    )
+    mt.write(output_mt_path)
+    logging.info(
+        f"Number of rare (freq<5%) and QC'd biallelic variants: {mt.count()[0]}"
+    )
+
+
+# endregion SUBSET_VARIANTS
+
+
+# region GET_GENE_SPECIFIC_VARIANTS
+
+
+def get_promoter_variants(
+    mt_path: str,  # ouput path from function above
+    ht_path: str,
+    gene_details: dict[str, str],  # ouput of make_gene_loc_dict
+    window_size: int,
+    plink_output_prefix: str,  # 'tob_wgs_rv/pseudobulk_rv_association/plink_files/'
+):
+    """Subset hail matrix table
+
+    Input:
+    mt_path: path to already subsetted hail matrix table
+    gene_file: path to a tsv file with information on
+    the chrom, start and end location of genes (rows)
+
+    Output:
+    For retained variants, that are: 1) in promoter regions and
+    2) within 50kb up or down-stream of the gene body (or in the gene body itself)
+    (on top of all filters done above)
+
+    returns:
+    - Path to plink prefix for genotype files (plink format: .bed, .bim, .fam)
+    - Path to hail table with variant (rows) stats for downstream analyses
+    """
+    # read hail matrix table object (pre-filtered)
+    init_batch()
+    mt = hl.read_matrix_table(mt_path)
+
+    gene_name = gene_details["gene_name"]
+
+    # get relevant chromosome
+    chrom = gene_details["chr"]
+
+    # subset to window
+    # get gene body position (start and end) and build interval
+    left_boundary = max(1, int(gene_details["start"]) - window_size)
+    right_boundary = min(
+        int(gene_details["end"]) + window_size,
+        hl.get_reference("GRCh38").lengths[f"chr{chrom}"],
+    )
+    # get gene-specific genomic interval
+    gene_interval = f"{chrom}:{left_boundary}-{right_boundary}"
+    logging.info(f"Interval considered: {gene_interval}")  # 'chr22:23219960-23348287'
+
+    # include variants up to {window size} up- and downstream
+    mt = hl.filter_intervals(
+        mt, [hl.parse_locus_interval(gene_interval, reference_genome="GRCh38")]
+    )
+    mt_path = output_path(f"{gene_name}_in_window.mt", "tmp")
+    mt = mt.checkpoint(
+        mt_path, overwrite=True
+    )  # add checkpoint to avoid repeat evaluation
+    logging.info(f"Number of variants within interval: {mt.count()[0]}")
+
+    # annotate using VEP
+    vep_ht = hl.read_table(ht_path)
+    mt = mt.annotate_rows(vep=vep_ht[mt.row_key].vep)
+
+    # filter variants found to be in promoter regions
+    mt = mt.filter_rows(
+        mt.vep.regulatory_feature_consequences["biotype"].contains("promoter")
+    )
+    promoter_path = output_path(f"{gene_name}promoter_variants.mt", "tmp")
+    mt = mt.checkpoint(promoter_path, overwrite=True)  # checkpoint
+    logging.info(
+        f"Number of rare (freq<5%) QC-passing, biallelic SNPs in promoter regions: {mt.count()[0]}"
+    )
+
+    # export this as a Hail table for downstream analysis
+    ht_path = output_path(f"{gene_name}_rare_promoter_summary.ht")
+    ht = mt.rows()
+    ht.write(ht_path)
+
+    # export MT object to PLINK (promoter variants)
+    plink_path = output_path(f"{plink_output_prefix}/{gene_name}")
+    export_plink(mt, plink_path, ind_id=mt.s)
+
+    return plink_path
+
+
+# endregion GET_GENE_SPECIFIC_VARIANTS
+
+# region MISCELLANEOUS
+
+
+def make_gene_loc_dict(file) -> dict[str, dict]:
+    """
+    Turn gene information into a dictionary
+    to avoid opening this file for every gene
+    """
+    gene_dict = {}
+
+    with open(file) as handle:
+        reader = DictReader(handle, delimiter="\t")
+
+        for row in reader:
+            gene_dict[row["gene_name"]] = row
+
+    return gene_dict
+
+
+# copied from https://github.com/populationgenomics/tob-wgs/blob/main/scripts/eqtl_hail_batch/launch_eqtl_spearman.py
+# check whether it needs modifying
+def remove_sc_outliers(df, outliers=["966_967", "88_88"]):
+    """
+    Remove outlier samples, as identified by single-cell analysis
+    """
+    # check syntax based on specific file
+    df = df[-df.sampleid.isin(outliers)]
+
+    return df
+
+
+# endregion MISCELLANEOUS
+
+
+@click.command()
+@click.option("--chromosomes")
+@click.option("--genes")
+@click.option("--celltypes")
+@click.option("--expression-file-prefix", default="scrna-seq/grch38_association_files")
+@click.option(
+    "--sample-mapping-file",
+    default="scrna-seq/grch38_association_files/OneK1K_CPG_IDs.tsv",
+)
+@click.option("--output-dir-path", default="tob_wgs_rv/pseudobulk_rv_association")
+@click.option("--mt-path", default="mt/v7.mt")
+@click.option("--anno-ht-path", default="tob_wgs_vep/104/vep104.3_GRCh38.ht")
+def crm_pipeline(
+    chromosomes: list[str],
+    genes: list[str],
+    celltypes: list[str],
+    expression_files_prefix: str,
+    sample_mapping_file: str,
+    output_dir_path: str,
+    mt_path: str = DEFAULT_JOINT_CALL_MT,
+    anno_ht_path: str = DEFAULT_ANNOTATION_HT,
+    window_size: int = 50000,
+):
+
+    sb = hb.ServiceBackend(
+        billing_project=get_config()["hail"]["billing_project"],
+        remote_tmpdir=remote_tmpdir(),
+    )
+    batch = hb.Batch("CellRegMap pipeline", backend=sb)
+
+    # extract samples for which we have single-cell (sc) data
+    sample_mapping_file = remove_sc_outliers(sample_mapping_file)
+    sc_samples = sample_mapping_file["InternalID"].unique()
+
+    # filter to QC-passing, rare, biallelic variants
+    filter_job = batch.new_python_job(name="MT filter job")
+    output_mt_path = output_path("tob_wgs_rv/densified_rv_only.mt")
+    filter_job.image(CELLREGMAP_IMAGE)
+    filter_job.call(
+        filter_variants,
+        mt_path=mt_path,
+        samples=sc_samples,
+        output_mt_path=output_mt_path,
+    )
+
+    # grab all relevant genes across all chromosomes
+    # simpler if gene details are condensed to one file
+    gene_dict: dict[str, dict] = {}
+    for chromosome in chromosomes:
+        geneloc_tsv_path = os.path.join(
+            expression_files_prefix,
+            "gene_location_files",
+            f"GRCh38_geneloc_chr{chromosome}.tsv",
+        )
+
+        # concatenating across chromosomes to have a single dict
+        gene_dict.update(make_gene_loc_dict(geneloc_tsv_path))
+
+    # isolate to the genes we are interested in
+    genes_of_interest = genes or list[gene_dict.keys()]
+
+    # for each gene, extract relevant variants (in window + with some annotation)
+    # submit a job for each gene (export genotypes to plink)
+    for gene in genes_of_interest:
+
+        # final path for this gene - generate first (check syntax)
+        plink_file = output_path(f"{output_dir_path}/plink_files/{gene}")
+        gene_dict[gene]["plink"] = plink_file
+
+        # if the plink output exists, do not re-generate it
+        if to_path(f"{plink_file}.bim").exists():
+            continue
+
+        plink_job = batch.new_python_job(f"Create plink files for: {gene}")
+        plink_job.depends_on(filter_job)
+        plink_job.image(CELLREGMAP_IMAGE)
+        plink_job.call(
+            get_promoter_variants,
+            mt_path=output_mt_path,
+            ht_path=anno_ht_path,
+            gene_details=gene_dict[gene],
+            window_size=window_size,
+            plink_file=plink_file,
+        )
+
+    # set jobs running
+    batch.run(wait=False)
+
+
+if __name__ == "__main__":
+    crm_pipeline()
