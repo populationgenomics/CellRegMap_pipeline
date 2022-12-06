8cd6d6c13d7655e8b5cd43345c2e586e5ab2ef78
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-12-06 16:17:31 +1100

    open str as df

--- batch.py
+++ batch.py
@@ -477,7 +477,8 @@ def summarise_association_results(
     one csv table per cell type,
     combining results across all genes in a single file
     """
-    pv_all_df = pd.concat(pv_dfs)
+    # pv_all_df = pd.concat(pv_dfs)
+    pv_all_df = pd.concat([pd.read_csv(AnyPath(pv_df), sep='\t') for pv_df in pv_dfs])
 
     # run qvalues for all tests
     pv_all_df['Q_CRM_VC'] = qvalue(pv_all_df['P_CRM_VC'])
