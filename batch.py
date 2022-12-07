ca83f47ca234b9b436e61698ca6d57ad41535287
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-12-07 17:21:50 +1100

    comment out more lines

--- batch.py
+++ batch.py
@@ -907,16 +907,16 @@ def crm_pipeline(
         # combine all p-values across all chromosomes, genes (per cell type)
         summarise_job = batch.new_python_job(f'Summarise all results for {celltype}')
         copy_common_env(summarise_job)
-        summarise_job.depends_on(*gene_run_jobs)
-        summarise_job.image(CELLREGMAP_IMAGE)
-        pv_all_filename_csv = str(output_path(f'{celltype}_all_pvalues.csv'))
-        print(pv_all_filename_csv)
-        print([gene_dict[gene]['pv_file'] for gene in genes_list])
-        summarise_job.call(
-            summarise_association_results,
-            *[gene_dict[gene]['pv_file'] for gene in genes_list],
-            pv_all_filename=str(pv_all_filename_csv),
-        )
+        # summarise_job.depends_on(*gene_run_jobs)
+        # summarise_job.image(CELLREGMAP_IMAGE)
+        # pv_all_filename_csv = str(output_path(f'{celltype}_all_pvalues.csv'))
+        # print(pv_all_filename_csv)
+        # print([gene_dict[gene]['pv_file'] for gene in genes_list])
+        # summarise_job.call(
+        #     summarise_association_results,
+        #     *[gene_dict[gene]['pv_file'] for gene in genes_list],
+        #     pv_all_filename=str(pv_all_filename_csv),
+        # )
 
     # set jobs running
     batch.run(wait=False)
