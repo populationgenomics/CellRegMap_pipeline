b260af9d1343741d874da819836e821153770ca0
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-12-07 16:46:04 +1100

    fix typo

--- batch.py
+++ batch.py
@@ -860,7 +860,7 @@ def crm_pipeline(
                 print("No plink files for this gene, exit!")
                 continue
             plink_output_prefix = gene_dict[gene]['plink']
-            prepare input files
+            # prepare input files
             prepare_input_job = batch.new_python_job(f'Prepare inputs for: {gene}')
             manage_concurrency_for_job(prepare_input_job)
             copy_common_env(prepare_input_job)
