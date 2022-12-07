eaf7a8ccdfd774b7a98b9700def0c5b25d30212a
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-12-07 14:59:05 +1100

    try to change dependencies

--- batch.py
+++ batch.py
@@ -863,9 +863,10 @@ def crm_pipeline(
             prepare_input_job = batch.new_python_job(f'Prepare inputs for: {gene}')
             manage_concurrency_for_job(prepare_input_job)
             copy_common_env(prepare_input_job)
-            plink_dep = gene_dict[gene].get('plink_job')
-            if plink_dep:
-                prepare_input_job.depends_on(plink_dep)
+            prepare_input_job.depends_on(*genotype_jobs)
+            # plink_dep = gene_dict[gene].get('plink_job')
+            # if plink_dep:
+            #     prepare_input_job.depends_on(plink_dep)
             prepare_input_job.image(CELLREGMAP_IMAGE)
             # the python_job.call only returns one object
             # the object is a file containing y_df, geno_df, kinship_df
