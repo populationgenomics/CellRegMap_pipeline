5f5a93b70148d7758805170dbc92081b2432e2b0
Anna Cuomo <anna.cuomo@populationgenomics.org.au>
2022-12-14 16:14:48 +1100

    Summarise using glob (#34)
    * change summarise to find pv files in a folder
    
    * format with black
    
    * turn into str to measure length?
    
    * reformat and delete commented-out lines
    
    * add statement to test length of list
    
    * make generator into list

--- batch.py
+++ batch.py
@@ -466,7 +466,8 @@ def run_gene_association(
 
 
 def summarise_association_results(
-    pv_dfs: list[str],
+    # pv_dfs: list[str],
+    pv_folder: str,
     pv_all_filename_str: str,
 ):
     """Summarise results
@@ -480,16 +481,12 @@ def summarise_association_results(
     """
     from multipy.fdr import qvalue
 
-    existing_pv_files = [pv_df for pv_df in pv_dfs if to_path(pv_df).exists()]
+    existing_pv_files = list(to_path(pv_folder).glob('*_pvals.csv'))
+
+    print(f'Number of files: {len(existing_pv_files)}')
 
     if len(existing_pv_files) == 0:
-        logging.info(f'tested: {pv_dfs}')
         raise Exception('No PV files, nothing to do')
-
-    logging.info(
-        f'Running on {len(existing_pv_files)} PV files, '
-        f"{len(pv_dfs) - len(existing_pv_files)} files didn't exist"
-    )
 
     pv_all_df = pd.concat(
         [pd.read_csv(to_path(pv_df), index_col=0) for pv_df in existing_pv_files]
@@ -754,14 +751,10 @@ def crm_pipeline(
             continue
 
         gene_run_jobs = []
-        pv_files = []
         for gene in genes_list:
 
             # wrapped this with output_path
             pv_file = output_path(f'{celltype}/{gene}_pvals.csv')
-
-            # always append the file name
-            pv_files.append(pv_file)
 
             # check if running is required
             if to_path(pv_file).exists():
@@ -808,14 +801,13 @@ def crm_pipeline(
         summarise_job = batch.new_python_job(f'Summarise all results for {celltype}')
         copy_common_env(summarise_job)
         summarise_job.depends_on(*gene_run_jobs)
-        # summarise_job.image(CELLREGMAP_IMAGE)
         summarise_job.image(MULTIPY_IMAGE)
         pv_all_filename_csv = str(
             output_path(f'{celltype}_all_pvalues.csv', 'analysis')
         )
         summarise_job.call(
             summarise_association_results,
-            pv_files,
+            pv_folder=output_path(celltype),
             pv_all_filename_str=str(pv_all_filename_csv),
         )
 
